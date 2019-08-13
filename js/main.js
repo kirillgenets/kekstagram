cb2d89d379ddb864d01f72956d782bc43add4ef7
Kirill Genets <kirill.genets2002@mail.ru>
2019-08-13 13:24:51 +0300

    pictures are in a document! yahooo!!!

--- js/main.js
+++ js/main.js
@@ -35,25 +35,9 @@ var messagesList = [
 
 var postedPhotos = [];
 
-makePhotosArray();
+insertPhotosIntoDocument();
 
 /* Функции */
-
-function makePhotosArray() {
-
-	for (var i = 0; i < 25; i++) {
-
-		postedPhotos.push({
-
-			url: 'photos/' + (i + 1) + '.jpg',
-			likes: Math.floor(Math.random() * (200 - 15) + 15),
-			comments: generateRandomComments()
-
-		});
-
-	}
-
-}
 
 function generateRandomComments() {
 
@@ -71,5 +55,45 @@ function generateRandomComments() {
 	}
 
 	return commentsList;
+
+}
+
+function makePhotosArray() {
+
+	for (var i = 0; i < 25; i++) {
+
+		postedPhotos.push({
+			url: 'photos/' + (i + 1) + '.jpg',
+			likes: Math.floor(Math.random() * (200 - 15) + 15),
+			comments: generateRandomComments()
+		});
+
+	}
+
+}
+
+function insertPhotosIntoDocument() {
+
+	makePhotosArray();
+
+	var photosList = document.querySelector('.pictures');
+	var photosListFragment = document.createDocumentFragment();
+
+	for (var i = 0; i < postedPhotos.length; i++) {
+		var pictureTemplate = document.querySelector('#picture');
+		var picture = pictureTemplate.content.querySelector('.picture__img');
+		var commentsCount = pictureTemplate.content.querySelector('.picture__comments');
+		var likesCount = pictureTemplate.content.querySelector('.picture__likes');
+
+		picture.setAttribute('src', postedPhotos[i].url);
+		commentsCount.textContent = postedPhotos[i].comments.length;
+		likesCount.textContent = postedPhotos[i].likes;
+
+		var pictureForDocument = document.importNode(pictureTemplate.content, true);
+
+		photosListFragment.appendChild(pictureForDocument);
+	}
+
+	photosList.appendChild(photosListFragment);
 
 }
