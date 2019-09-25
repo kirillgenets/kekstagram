c5c5375e9ac079e5d6bfe44e8a41ad4657919512
Kirill Genets <kirill.genets2002@mail.ru>
2019-09-25 08:46:51 +0300

    fixed some bugs in draw-all-pictures.js

--- js/draw-all-pictures.js
+++ js/draw-all-pictures.js
@@ -2,57 +2,45 @@
 
 (function () {
 
-  drawPictures();
+  var pictureTemplate = document.querySelector('#picture').content.querySelector('.picture');
+  var pictures = document.querySelector('.pictures');
+  var picturesFragment = document.createDocumentFragment();
 
-  function drawPictures() {
-    insertPhotosIntoDocument();
+  window.data.forEach(function (picture, index) {
+    picturesFragment.appendChild(createPicture(picture, index));
+  });
 
-    function insertPhotosIntoDocument() {
-      var pictureTemplate = document.querySelector('#picture').content.querySelector('.picture');
-      var pictures = document.querySelector('.pictures');
-      var picturesFragment = document.createDocumentFragment();
+  pictures.appendChild(picturesFragment);
 
-      window.data.forEach(function (picture, index) {
-        picturesFragment.appendChild(createPicture(picture, index));
-      });
+  var smallPictures = document.querySelectorAll('.picture');
 
-      pictures.appendChild(picturesFragment);
+  smallPictures.forEach(initPicturesListeners);
 
-      var smallPictures = document.querySelectorAll('.picture');
+  function createPicture(picture, index) {
+    var image = pictureTemplate.cloneNode(true);
 
-      smallPictures.forEach(initPicturesListeners);
+    image.setAttribute('data-number', index);
+    image.querySelector('.picture__comments').textContent = picture.comments.length; // Устанавливаем количество комментариев
+    image.querySelector('.picture__likes').textContent = picture.likes; // Устанавливаем количество лайков
+    image.querySelector('.picture__img').src = picture.url; // Устанавливаем картинку
 
-      function createPicture(picture, index) {
-        var image = pictureTemplate.cloneNode(true);
-
-        image.setAttribute('data-number', index);
-        image.querySelector('.picture__comments').textContent = picture.comments.length; // Устанавливаем количество комментариев
-        image.querySelector('.picture__likes').textContent = picture.likes; // Устанавливаем количество лайков
-        image.querySelector('.picture__img').src = picture.url; // Устанавливаем картинку
-
-        return image;
-      }
-
-      function initPicturesListeners(picture) {
-        picture.addEventListener('click', onPictureClick);
-        picture.addEventListener('keydown', onPictureKeyDown);
-
-        function onPictureClick(evt) {
-          window.drawBigPicture(getNumberOfPicture(evt.currentTarget));
-        }
-
-        function onPictureKeyDown(evt) {
-          if (window.utilities.isEnterEvent(evt)) {
-            evt.preventDefault();
-            window.drawBigPicture(getNumberOfPicture(evt.currentTarget));
-          }
-        }
-      }
-    }
+    return image;
   }
 
-  function getNumberOfPicture(picture) {
-    return picture.dataset.number;
+  function initPicturesListeners(picture) {
+    picture.addEventListener('click', onPictureClick);
+    picture.addEventListener('keydown', onPictureKeyDown);
+
+    function onPictureClick(evt) {
+      window.drawBigPicture(evt.currentTarget.dataset.number);
+    }
+
+    function onPictureKeyDown(evt) {
+      if (window.utilities.isEnterEvent(evt)) {
+        evt.preventDefault();
+        window.drawBigPicture(evt.currentTarget.dataset.number);
+      }
+    }
   }
 
 })();
